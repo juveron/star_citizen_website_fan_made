da8cec597f66483247622727ea45e10a5ac22835
Jules Elferink <j.e.veron77@gmail.com>
2024-12-02 13:57:10 +0100

    clean code: Change the localisation of logic
    Moving provideHttpClient in appconfig

--- star_citizen_website_fan_made/src/app/app.config.ts
+++ star_citizen_website_fan_made/src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { appRoutes } from './core/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+    provideHttpClient()
+  ]
 };
--- star_citizen_website_fan_made/src/main.ts
+++ star_citizen_website_fan_made/src/main.ts
@@ -1,12 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { appRoutes } from './app/core/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(appRoutes),
-    provideHttpClient()
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
